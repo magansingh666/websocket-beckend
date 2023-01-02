845fc46ebd613ed3028af54005ee8047e9139835
Magan Singh <magansingh666@gmail.com>
2023-01-02 13:29:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+let pg = require('pg');
+let env1 = require('dotenv')
+env1.config()
+
+
+let conString = process.env.DB_URL
+console.log(conString)
+let client = new pg.Client(conString);
+client.connect(function(err) {
+  if(err) {
+    return console.error('could not connect to postgres', err);
+  }
+  client.query('SELECT NOW() AS "theTime"', function(err, result) {
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0].theTime);
+    // >> output: 2018-08-23T14:02:57.117Z
+    client.end();
+  });
+})
